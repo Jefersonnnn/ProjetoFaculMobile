c807765bb9a9d8426bb6905f42c210fed6dd8c32
Jeferson Machado <jefersonmachado@univille.br>
2016-08-03 23:52:14 -0300

    Inicial

---
+++ app/src/main/java/univille/com/br/loggermarker/univille/com/br/loggermarker/view/ActSplashScreen.java
@@ -0,0 +1,26 @@
+package univille.com.br.loggermarker.univille.com.br.loggermarker.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import univille.com.br.loggermarker.R;
+
+/**
+ * Created by Jeferson Machado on 03/08/2016.
+ */
+
+
+/**
+ * <b>Classe ActSplashScreen</b>
+ * Classe para atribuir o xml act_splash_screen.xml
+ */
+public class ActSplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_splash_screen);
+
+
+    }
+}
