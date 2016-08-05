92ff4b0622ffb84db10a3a2b99b7cee629ff40f0
Jeferson Machado <jefersonmachado@univille.br>
2016-08-05 20:32:31 -0300

    #11 Primeira parte

---
+++ app/src/main/java/univille/com/br/loggermarker/univille/com/br/loggermarker/view/ActSubProjeto.java
@@ -0,0 +1,15 @@
+package univille.com.br.loggermarker.univille.com.br.loggermarker.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import univille.com.br.loggermarker.R;
+
+public class ActSubProjeto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_sub_projeto);
+    }
+}
