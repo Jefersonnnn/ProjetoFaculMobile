9ccb664b269cc015da25c5b5c48b183212de63a0
felipeprestess <felipeadriano_pc@hotmail.com>
2016-08-06 00:27:30 -0300

    #14 implementação da classe

---
+++ app/src/main/java/univille/com/br/loggermarker/model/Logger.java
@@ -0,0 +1,23 @@
+class Logger{
+
+    public Logger(){
+        
+    }
+
+    private int cdLogger;
+    private String statusLogger;
+
+
+    public int getCdLogger(){
+        return cdLogger;
+    }
+
+    public String getStatusLogger(){
+        return statusLogger;
+    }
+
+    public void setStatusLogger(String statusLogger){
+        this.statusLogger = statusLogger;
+    }  
+
+}
